0999e509951209a89f1da78a46c83315dfb50a96
stevenpfaff <pfaffy1426@gmail.com>
2021-09-27 14:51:01 -0500

    models Employee class

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 app_name = "employees"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('edit_employee/', views.edit_employee, name="edit_employee"),
 ]
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+from django.db.models.manager import EmptyManager
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
-
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from .models import Employee
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
@@ -11,3 +14,36 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+@login_required
+def create_employee(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        weekly_from_form = request.POST.get('weekly_pickup')
+        new_customer = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form, weekly_pickup=weekly_from_form)
+        new_customer.save()
+        return HttpResponseRedirect(reverse('customers:index'))
+    else:
+        return render(request, 'customers/create.html')
+        
+@login_required
+def edit_employee(request):
+    logged_in_user = request.user
+    logged_in_employee = Employee.objects.get(user=logged_in_user)
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        logged_in_employee.name = name_from_form
+        logged_in_employee.address = address_from_form
+        logged_in_employee.zip_code = zip_from_form
+        logged_in_employee.save()
+        return HttpResponseRedirect(reverse('customers:index'))
+    else:
+        context = {
+            'logged_in_customer': logged_in_employee
+        }
+        return render(request, 'customers/edit_profile.html', context)
