a4b12964cd515fa64cef9a99ca0e1feda31692c6
KevinCosma <kcosman2005@gmail.com>
2021-09-27 14:53:12 -0500

    started working ont the employee index.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.apps import apps
+from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from datetime import date
+
+from .models import Employee    
 
 # Create your views here.
 
@@ -9,5 +14,19 @@ from django.apps import apps
 
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customers.Customer')
+    CustomerE = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+
+@login_required
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form, weekly_pickup=weekly_from_form)
+        new_employee.save()
+         return HttpResponseRedirect(reverse('customers:index'))
+    else:
+        return render(request, 'customers/create.html')
